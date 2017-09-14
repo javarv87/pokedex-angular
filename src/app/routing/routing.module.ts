4e3794bb0fba592c6fb5068c9ee7b4bb01e229f6
Javier Ruiz <javier.ruiz@mobilepro.mx>
2017-09-14 16:29:49 -0500

    Adding routing

---
+++ src/app/routing/routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PokemonListComponent } from '../pokemon-list/pokemon-list.component';
+import { PokemonDetailComponent } from '../pokemon-detail/pokemon-detail.component';
+
+const routes: Routes = [
+  { path: '', component: PokemonListComponent },
+  { path: 'details/:id', component: PokemonDetailComponent }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ],
+  declarations: []
+})
+export class RoutingModule { }
