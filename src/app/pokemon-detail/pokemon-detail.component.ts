e045aa6de531825a33ddd21ade176535c53d27c4
Javier Ruiz <javier.ruiz@mobilepro.mx>
2017-09-14 16:31:51 -0500

    Adding component pokemonDetail

---
+++ src/app/pokemon-detail/pokemon-detail.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Pokemon } from '../pokemon';
+import { PokemonService } from '../pokemon-service.service';
+import 'rxjs/add/operator/switchMap';
+
+@Component({
+  selector: 'app-pokemon-detail',
+  templateUrl: './pokemon-detail.component.html',
+  styleUrls: ['./pokemon-detail.component.css'],
+  providers: [PokemonService]
+})
+export class PokemonDetailComponent implements OnInit {
+  pokemon: Pokemon = new Pokemon();
+
+  constructor(private pokemonService: PokemonService, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.paramMap
+      .switchMap((params: ParamMap) => this.pokemonService.get(+params.get('id')))
+      .subscribe(pokemon => this.pokemon = pokemon);
+  }
+
+}
