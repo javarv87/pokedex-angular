cdecaed41f47c37742286f1ca610502ac715dd01
Javier Ruiz <javier.ruiz@mobilepro.mx>
2017-09-14 16:31:31 -0500

    Adding component pokemonList

---
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from '../pokemon';
+import { PokemonService } from '../pokemon-service.service';
+
+@Component({
+  selector: 'app-pokemon-list',
+  moduleId: module.id,
+  templateUrl: './pokemon-list.component.html',
+  styleUrls: ['./pokemon-list.component.css'],
+  providers: [PokemonService]
+})
+export class PokemonListComponent implements OnInit {
+  pokemons: Pokemon[];
+
+  constructor(private pokemonService: PokemonService) { }
+
+  ngOnInit(): void {
+    this.pokemonService.list()
+      .then((pokemons) => {
+        this.pokemons = pokemons;
+      });
+  }
+
+}
