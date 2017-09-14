fa4275cac066739252d3d9e548fbab55b500d536
Javier Ruiz <javier.ruiz@mobilepro.mx>
2017-09-14 18:14:27 -0500

    Adding material design styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { RoutingModule } from './routing/routing.module';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
+
+import 'hammerjs';
 
 @NgModule({
   declarations: [
@@ -15,10 +20,12 @@ import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.componen
     PokemonDetailComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     RoutingModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
